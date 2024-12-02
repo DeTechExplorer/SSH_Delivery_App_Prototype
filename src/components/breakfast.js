114e782f479c744589b60e4bd4e0ac71ccd5ee96
DeTechExplorer <ayshafawzi9@gmail.com>
2024-12-02 12:58:32 +0400

    removed duplicate header part

--- src/components/breakfast.js
+++ src/components/breakfast.js
@@ -284,24 +284,10 @@ function BreakfastPage() {
         </div>
       </header>
 
-      
-      <div className="location-bar">
-        SSH Delivery
-      </div>
-      <header className="top-bar">
-        <div className="logo-search-location">
-          <div id="logo">
-            <img src="./images/WhatsApp Image 2024-11-27 at 9.58.28 AM.jpeg" alt="Logo" id="logo-img" />
-          </div>
-          <div id="search-box">
-            <input type="text" placeholder="Search breakfast items..." />
-          </div>
-        </div>
-        <div id="location-btn-container">
-          <span id="location-text">Deliver to:</span>
-          <button id="location-btn">SSH Home London, UK</button>
-        </div>
-      </header>
+
+
+
+     
     </>
   );
 }
