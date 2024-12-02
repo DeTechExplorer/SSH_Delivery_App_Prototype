64890646560ded8ac9024ebb828374eeabe554ce
DeTechExplorer <ayshafawzi9@gmail.com>
2024-12-02 13:38:23 +0400

    basic structure of page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';  // Import necessary components
 import BakeryPage from './components/bakery';
 import BreakfastPage from './components/breakfast';
+import FruitsAndVegPage from './components/fruitsandveg';  // Import your new Fruits and Veg component
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
             <li>
               <Link to="/breakfast">Breakfast</Link>
             </li>
+            <li>
+              <Link to="/fruitsandveg">Fruits & Vegetables</Link> {/* New link */}
+            </li>
           </ul>
         </nav>
 
@@ -26,6 +30,9 @@ function App() {
 
           {/* Route for Breakfast Page */}
           <Route path="/breakfast" element={<BreakfastPage />} />
+
+          {/* Route for Fruits and Vegetables Page */}
+          <Route path="/fruitsandveg" element={<FruitsAndVegPage />} /> {/* New route */}
         </Routes>
       </div>
     </Router>
