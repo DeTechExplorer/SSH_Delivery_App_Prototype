4217dd22f5bacfe2827135a75b964af00b47a672
Aysha Gadim <172500251+DeTechExplorer@users.noreply.github.com>
2024-12-02 22:17:13 +0400

    Update productsData.js
    added promotion items to the list to call when promotion banner is clicked to display the items

--- src/components/productsData.js
+++ src/components/productsData.js
@@ -1044,6 +1044,154 @@ export const productsData = {
         ]
       }
     };
+export const promotionsData = {
+  id: 'promotions',
+  name: 'Special Offers - 20% Off',
+  products: [
+    {
+      id: 'promo-001',
+      name: 'Chocolate Cake',
+      originalPrice: 18.99,
+      discountedPrice: 15.19,
+      unit: 'cake',
+      image: 'https://bonnibakery.com/wp-content/uploads/2021/01/ChocolateCake-2-scaled.jpg',
+      discount: 20,
+      category: 'promotions',
+      inStock: true
+    },
+    {
+      id: 'promo-002',
+      name: 'Classic Cheesecake',
+      originalPrice: 16.99,
+      discountedPrice: 13.59,
+      unit: 'cake',
+      image: 'https://stateofdinner.com/wp-content/uploads/2023/03/no-bake-vanilla-cheesecake-featured-2-500x375.jpg',
+      discount: 20,
+      category: 'promotions',
+      inStock: true
+    },
+    {
+      id: 'promo-003',
+      name: 'Sausages',
+      originalPrice: 3.25,
+      discountedPrice: 2.06,
+      unit: 'pack',
+      image: 'https://images.cdn.shoprite.com/detail/00077782030181_1',
+      discount: 20,
+      category: 'promotions',
+      inStock: true
+    },
+    {
+      id: 'promo-004',
+      name: 'Cottage Cheese',
+      originalPrice: 3.20,
+      discountedPrice: 2.56,
+      unit: 'pack',
+      image: 'https://pngimg.com/d/cottage_cheese_PNG1.png',
+      discount: 20,
+      category: 'promotions',
+      inStock: true
+    },
+    {
+      id: 'promo-005',
+      name: 'Ice lollies',
+      originalPrice: 2.80,
+      discountedPrice: 2.24,
+      unit: 'pack',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQieO0lS3EATr1FEHvH5ArVfyphqUJBHbluIg&s',
+      discount: 20,
+      category: 'promotions',
+      inStock: true
+    },
+    {
+      id: 'promo-006',
+      name: 'Pepsi',
+      originalPrice: 2.00,
+      discountedPrice: 1.60,
+      unit: 'can',
+      image: 'https://marino.ae/cdn/shop/files/Pepsi-Can-250-ml-30x250ml-_1-carton_-Marino.AE-234557742.jpg?v=1707548947&width=750',
+      discount: 20,
+      category: 'promotions',
+      inStock: true
+    },
+    {
+      id: 'promo-007',
+      name: 'Soap',
+      originalPrice: 1.80,
+      discountedPrice: 1.44,
+      unit: 'bar',
+      image: 'https://marino.ae/cdn/shop/files/DETTOL-SOAP-SKINCARE-165GMx48-_ARABIC_-_1-carton_-Marino.AE-215398875.jpg?v=1706689942',
+      discount: 20,
+      category: 'promotions',
+      inStock: true
+    },
+    {
+      id: 'promo-008',
+      name: 'Ground Black Pepper',
+      originalPrice: 1.30,
+      discountedPrice: 1.04,
+      unit: '50g',
+      image: 'https://digitalcontent.api.tesco.com/v2/media/ghs/22fb224a-1658-4e34-8ea4-59eaa1812ce1/9ae31267-0e37-4d3b-a720-96184d2cd3de_1296891052.jpeg?h=960&w=960',
+      discount: 20,
+      category: 'promotions',
+      inStock: true
+    },
+    {
+      id: 'promo-009',
+      name: 'Strawberry',
+      originalPrice: 2.50,
+      discountedPrice: 2.00,
+      unit: 'punnet',
+      image: 'https://pngimg.com/d/strawberry_PNG2598.png',
+      discount: 20,
+      category: 'promotions',
+      inStock: true
+    },
+    {
+      id: 'promo-010',
+      name: 'Teabags',
+      originalPrice: 2.50,
+      discountedPrice: 2.00,
+      unit: '80 pack',
+      image: 'https://m.media-amazon.com/images/I/713C+S18uSL._AC_UF894,1000_QL80_.jpg',
+      discount: 20,
+      category: 'promotions',
+      inStock: true
+    },
+    {
+      id: 'promo-011',
+      name: 'Olive Oil',
+      originalPrice: 3.99,
+      discountedPrice: 3.19,
+      unit: '500ml',
+      image: 'https://m.media-amazon.com/images/I/41rhraxOdWL.jpg',
+      discount: 20,
+      category: 'promotions',
+      inStock: true
+    },
+    {
+      id: 'promo-012',
+      name: 'Shampoo',
+      originalPrice: 3.50,
+      discountedPrice: 2.80,
+      unit: 'bottle',
+      image: 'https://m.media-amazon.com/images/I/51Be9C6cvTS.jpg',
+      discount: 20,
+      category: 'promotions',
+      inStock: true
+    }
+  ]
+};
+
+
+export const getPromotions = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(promotionsData);
+    }, 300);
+  });
+};
+ 
  
 
 // Helper functions to simulate API calls
