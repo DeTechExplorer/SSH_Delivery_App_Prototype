ff9e47155d28b828f6a44c7628848a3b3ba1d1ec
Madiha <s.madihanaz31@gmail.com>
2024-12-04 23:32:24 +0400

    added fetch function

--- src/components/snack.js
+++ src/components/snack.js
@@ -9,4 +9,19 @@ function SnackPage() {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
-}  
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+          try {
+            const data = await getProductsByCategory('snacks'); // Fetch snack products
+            setProducts(data.products);
+          } catch (error) {
+            console.error('Error fetching snack products:', error);
+          } finally {
+            setLoading(false);
+          }
+        };
+    
+        fetchProducts();
+      }, []);
+    
