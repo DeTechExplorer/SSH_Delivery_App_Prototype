3276d5fe90476b24183f54a14c6eff2df9d51d45
Madiha <s.madihanaz31@gmail.com>
2024-12-04 23:30:05 +0400

    edited App.js file and added imports

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import SnackPage from './components/snack';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/category/snack">View Snack Products</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<DairyPage />} />
+          <Route path="/category/snack" element={<SnackPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/snack.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getProductsByCategory } from './productsData';
+import logo from '../images/logo.jpeg';
