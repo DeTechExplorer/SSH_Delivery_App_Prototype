bb8324ca3f7932d971ddf20043b8db10ce78dece
sreyavnd <sreya.v.kumar@gmail.com>
2024-12-04 19:52:34 +0400

    initialization

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
 import InvoiceForm from './components/cardpayment';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DairyPage from './components/dairy'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <InvoiceForm />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          {/* Define routes for different pages */}
+          <Routes>
+            <Route path="/" element={<InvoiceForm />} /> {/* Default route */}
+            <Route path="/dairy" element={<DairyPage />} /> {/* DairyPage route */}
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
