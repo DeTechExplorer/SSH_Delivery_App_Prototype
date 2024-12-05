489b48de58ac531608f5c090a994c31aedd34f4c
DeTechExplorer <ayshafawzi9@gmail.com>
2024-12-06 01:44:10 +0400

    Bakery page logo

--- src/components/bakery.js
+++ src/components/bakery.js
@@ -365,7 +365,7 @@ useEffect(() => {
       <header className="top-bar">
         <div className="logo-search-location">
           <div id="logo">
-            <img src="/api/placeholder/130/130" alt="Logo" id="logo-img" />
+            <img src={Logo} alt="Logo" id="logo-img" />
           </div>
           <div id="search-box">
             <input type="text" placeholder="Search bakery items..." />
