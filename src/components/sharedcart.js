1b9d525c47fa3ba310044e8af2b49e206963a6fb
DeTechExplorer <ayshafawzi9@gmail.com>
2024-12-06 01:46:12 +0400

    Shared order page logo

--- src/components/sharedcart.js
+++ src/components/sharedcart.js
@@ -650,7 +650,7 @@ function CartPage() {
       <header className="top-bar">
         <div className="logo-search-location">
           <div id="logo">
-            <img src="/api/placeholder/130/130" alt="Logo" id="logo-img" />
+            <img src={Logo} alt="Logo" id="logo-img" />
           </div>
           <div id="search-box">
             <input type="text" placeholder="Search items..." />
