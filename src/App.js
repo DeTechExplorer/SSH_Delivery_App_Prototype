08e1627f84e1b93693a6efc383c2053e24840253
sreyavnd <sreya.v.kumar@gmail.com>
2024-12-06 11:20:39 +0400

    imports for dairy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import InvoiceForm from './components/cardpayment'; // Import InvoiceForm
 import FeedbackForm from './components/feedback'; // Import FeedbackForm
-
+import DairyPage from './components/dairy'; // Import DairyPage
 function App() {
   return (
     <Router>
@@ -17,6 +17,9 @@ function App() {
             <li>
               <Link to="/feedback">Give Feedback</Link> {/* Navigation to Feedback Form */}
             </li>
+            <li>
+              <Link to="/category/dairy">Dairy Products</Link> {/* Navigation to Dairy Products Page */}
+            </li>
             
           </ul>
         </nav>
@@ -26,6 +29,7 @@ function App() {
           <Route path="/" element={<InvoiceForm />} /> {/* Default route to Invoice Form */}
           <Route path="/invoice" element={<InvoiceForm />} /> {/* Route for Invoice Form */}
           <Route path="/feedback" element={<FeedbackForm />} /> {/* Route for Feedback Form */}
+          <Route path="/category/dairy" element={<DairyPage />} /> {/* Route for Dairy Products */}
           
         </Routes>
       </div>
