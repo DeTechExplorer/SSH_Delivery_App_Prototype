f078920a990605629df1b57d90fa14abffe26cf0
sreyavnd <sreya.v.kumar@gmail.com>
2024-12-06 11:46:52 +0400

    added bottom navigation bar for baeuty page

--- src/components/beauty.js
+++ src/components/beauty.js
@@ -390,7 +390,14 @@ return (
 
 
   
-    
+    <div className="bottom-nav">
+      <button onClick={() => navigate('/')}>Categories</button>
+      <button onClick={() => navigate('/')}>Home</button>
+      <button id="cart-btn">
+        <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt="Cart" />
+        Cart ({cartCount})
+      </button>
+    </div>
   </>
 );
 }
