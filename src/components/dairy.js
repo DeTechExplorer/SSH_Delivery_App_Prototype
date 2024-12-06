760b0b56e28517c5a725cfc406bb2d9b6bf29a2b
sreyavnd <sreya.v.kumar@gmail.com>
2024-12-06 11:21:55 +0400

    fetching products

--- src/components/dairy.js
+++ src/components/dairy.js
@@ -2,3 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getProductsByCategory } from './productsData';
 import logo from './images/LOGO.jpeg';
+useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const data = await getProductsByCategory('dairy');
+        setProducts(data.products);
+      } catch (error) {
+        console.error('Error fetching dairy products:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
