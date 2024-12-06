b0d12bc964ad8f9913893b5b7383ea99a1e4a106
sreyavnd <sreya.v.kumar@gmail.com>
2024-12-06 11:34:13 +0400

    initialization of beauty and personal care page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import InvoiceForm from './components/cardpayment'; // Import InvoiceForm
-import FeedbackForm from './components/feedback'; // Import FeedbackForm
-import DairyPage from './components/dairy'; // Import DairyPage
+import InvoiceForm from './components/cardpayment'; 
+import FeedbackForm from './components/feedback'; 
+import DairyPage from './components/dairy'; 
+import BeautyPage from './components/beauty';
+
 function App() {
   return (
     <Router>
@@ -17,9 +19,7 @@ function App() {
             <li>
               <Link to="/feedback">Give Feedback</Link> {/* Navigation to Feedback Form */}
             </li>
-            <li>
-              <Link to="/category/dairy">Dairy Products</Link> {/* Navigation to Dairy Products Page */}
-            </li>
+            
             
           </ul>
         </nav>
@@ -30,6 +30,7 @@ function App() {
           <Route path="/invoice" element={<InvoiceForm />} /> {/* Route for Invoice Form */}
           <Route path="/feedback" element={<FeedbackForm />} /> {/* Route for Feedback Form */}
           <Route path="/category/dairy" element={<DairyPage />} /> {/* Route for Dairy Products */}
+          <Route path="/category/beauty" element={<BeautyPage />} />  {/* Route for Beauty Page */}
           
         </Routes>
       </div>
